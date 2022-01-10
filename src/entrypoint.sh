e31370a7055124b4d0fa21250f6451327c7eb2b8
Jan Brinkmann <jan.brink@openpi.de>
2022-01-10 20:49:16 +0100

    Allow separate variables.

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,8 +2,21 @@
 
 set -e
 
+HOURLY="${HOURLY:-0}"
+DAILY="${DAILY:-7}"
+WEEKLY="${WEEKLY:-4}"
+MONTHLY="${MONTHLY:-12}"
+YEARLY="${YEARLY:-always}"
+DRY_RUN="${DRY_RUN:-true}"
+
 if [ -z $ROTATION_SCHEME ]; then
-  ROTATION_SCHEME="--daily=7 --weekly=4 --monthly=12 --yearly=always --dry-run"
+  ROTATION_SCHEME="--hourly=$HOURLY --daily=$DAILY --weekly=$WEEKLY --monthly=$MONTHLY --yearly=$YEARLY"
+  if [ $DRY_RUN == true ]; then
+    ROTATION_SCHEME="$ROTATION_SCHEME --dry-run"
+  fi
 fi
+
+echo -e "\n\033[1m[INFO] rotate-backups \033[0m \n"
+echo "ROTATION_SCHEME: $ROTATION_SCHEME"
 
 /usr/local/bin/rotate-backups $ROTATION_SCHEME /archive
